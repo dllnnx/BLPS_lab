7b73fd7a302942ea7290b8db81aaed8c0b5c0051
p11menova <p11menova@yandex.ru>
2026-04-11 18:05:02 +0300

    removed unused properties params

--- payment-service/src/main/java/ru/itmo/configurations/JmsConfig.java
+++ payment-service/src/main/java/ru/itmo/configurations/JmsConfig.java
@@ -1,18 +1,15 @@
 package ru.itmo.configurations;
 
 import jakarta.jms.ConnectionFactory;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
-@Slf4j
 @Configuration
 public class JmsConfig {
 
@@ -25,13 +22,14 @@ public class JmsConfig {
     @Value("${activemq.password}")
     private String password;
 
-    @Bean(name = "openWireConnectionFactory")
-    @Primary
-    public ConnectionFactory openWireConnectionFactory() {
-        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(brokerUrl);
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        ActiveMQConnectionFactory factory =
+                new ActiveMQConnectionFactory(brokerUrl);
+
         factory.setUserName(username);
         factory.setPassword(password);
-        log.info("JMS: ActiveMQConnectionFactory over OpenWire (brokerURL={})", brokerUrl);
+
         return factory;
     }
 
