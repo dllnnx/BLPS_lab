67a85e579885e6d192aea7dac502d99d3b920022
dllnnx <alenadenisova528@gmail.com>
2026-05-23 15:30:29 +0300

    try but not everything works

--- payment-service/src/main/java/ru/itmo/PaymentApplication.java
+++ payment-service/src/main/java/ru/itmo/PaymentApplication.java
@@ -3,10 +3,18 @@ package ru.itmo;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @OpenAPIDefinition
-public class PaymentApplication {
+public class PaymentApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PaymentApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
     }
